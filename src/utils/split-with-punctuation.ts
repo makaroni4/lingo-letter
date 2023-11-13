c6f45d0158f7f661c5c80f57d7c2e0df6e6dfa7d
Anatoli Makarevich <makaroni4@gmail.com>
2023-11-13 20:47:39 +0100

    Added comments to the splitting function

--- src/utils/split-with-punctuation.ts
+++ src/utils/split-with-punctuation.ts
@@ -1,4 +1,8 @@
 export const splitWithPunctuation = (inputString: string): string[] => {
+  // Here we split a sente into words and punctutation:
+  //
+  // * we match individual words from unicode letters and a hyphen (-): [\p{L}-]+
+  // * we match non-letter and non-space tokens: [^\p{L}\s]
   const regex = /([\p{L}-]+|[^\p{L}\s])/gu;
 
   const result = inputString.match(regex) || [];
