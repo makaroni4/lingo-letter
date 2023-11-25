a2e7b99071530aaed0fd670883f8c19d940e00ce
Anatoli Makarevich <makaroni4@gmail.com>
2023-11-25 19:13:54 +0100

    Adjusted button font

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import { MouseEventHandler } from "react";
 export default function Button({ onClick = () => {}, children, className }: { className?: string, children: any, onClick?: MouseEventHandler }) {
   return (
     <button
-      className={`py-2 px-4 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md ${className}`}
+      className={`py-2 px-4 bg-indigo-500 font-medium	hover:bg-indigo-700 text-white rounded-md ${className}`}
       onClick={onClick}>
       { children }
     </button>
