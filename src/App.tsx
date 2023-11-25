a4d22388e1457b8f00b0218f1ccc4eb2596ebfbe
Anatoli Makarevich <makaroni4@gmail.com>
2023-11-25 13:08:22 +0100

    Extracted topic verification into a helper method, don't use async/await for verifying email and topics

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Settings from "./components/Settings"
 import Navbar from './components/Navbar';
 import { generateIncomingEmail } from './utils/generate-incoming-email';
 import { verifyEmailSubmission } from './utils/verify-email-submission';
+import { verifyTopics } from './utils/verify-topics';
 import { useTranslation } from "react-i18next";
 
 function App() {
@@ -78,46 +79,21 @@ function App() {
 
     setOriginalSentences(splitIntoSentences(letter))
 
-    const verifiedEmail = await verifyEmailSubmission({
+    verifyEmailSubmission({
       apiKey: openAIAPIKey,
       letter
+    }).then(verifiedEmail => {
+      const fixedSentences = splitIntoSentences(verifiedEmail)
+      setVerifiedSentences(fixedSentences)
     })
-    const fixedSentences = splitIntoSentences(verifiedEmail)
 
-    setVerifiedSentences(fixedSentences)
-
-    // Ask for topics
-
-    const topicsChatCompletion = await openai.chat.completions.create({
-      messages: [{
-        role: 'user',
-        content: `
-        I have a German letter here between ''' symbols:
-
-        '''${letter}'''
-
-
-        Are the following topics directly covered in the letter: ${responseTopics.join(', ')}? Rate each topic on the following scale:
-
-        0 – not covered at all.
-        1 - the topic is briefly mentioned in the letter.
-        2 - there're at least one sentence with 2 parts in the letter about the topic. There're also 1-2 not so complex sentences around the topic.
-
-        Respond in the JSON format and comment on what could I have done better for each topic:
-
-        {
-          TOPIC: {
-            grade: 0/1/2,
-            comment: ""
-          }
-        }
-      `
-      }],
-      model: 'gpt-3.5-turbo',
-    });
-
-    const topicsResponseMessage = topicsChatCompletion.choices[0].message
-    setTopicsVerification(JSON.parse(topicsResponseMessage.content || "{}"))
+    verifyTopics({
+      apiKey: openAIAPIKey,
+      topics: responseTopics,
+      letter
+    }).then(verifiedTopics => {
+      setTopicsVerification(verifiedTopics);
+    })
   }
 
   const topicGradeBgColor = (grade: number | undefined): string => {
---
+++ src/utils/verify-topics.ts
@@ -0,0 +1,44 @@
+import OpenAI from 'openai';
+
+export const verifyTopics = async ({ apiKey, letter, topics }: { apiKey: string | undefined, letter: string, topics: string[] }) => {
+  if (!apiKey) {
+    alert("Please, set Open AI API key")
+  }
+
+  const openai = new OpenAI({
+    apiKey,
+    dangerouslyAllowBrowser: true
+  });
+
+  const topicsChatCompletion = await openai.chat.completions.create({
+    messages: [{
+      role: 'user',
+      content: `
+      I have a German letter here between ''' symbols:
+
+      '''${letter}'''
+
+
+      Are the following topics directly covered in the letter: ${topics.join(', ')}? Rate each topic on the following scale:
+
+      0 – not covered at all.
+      1 - the topic is briefly mentioned in the letter.
+      2 - there're at least one sentence with 2 parts in the letter about the topic. There're also 1-2 not so complex sentences around the topic.
+
+      Respond in the JSON format and comment on what could I have done better for each topic:
+
+      {
+        TOPIC: {
+          grade: 0/1/2,
+          comment: ""
+        }
+      }
+    `
+    }],
+    model: 'gpt-3.5-turbo',
+  });
+
+  const topicsResponseMessage = topicsChatCompletion.choices[0].message
+
+  return JSON.parse(topicsResponseMessage.content || "{}")
+}
