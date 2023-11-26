775c9a87294ac0ef7f651def249a37e8b5ab17dd
Anatoli Makarevich <makaroni4@gmail.com>
2023-11-26 20:03:31 +0100

    Added react-markdown

--- src/components/WelcomePopup.tsx
+++ src/components/WelcomePopup.tsx
@@ -1,6 +1,8 @@
+import { Fragment } from 'react';
 import { useAppStore } from '../store';
 import { useTranslation } from "react-i18next";
 import { XMarkIcon } from '@heroicons/react/24/solid';
+import Markdown from 'react-markdown';
 
 export default function WelcomePopup() {
   const { t } = useTranslation();
@@ -9,6 +11,14 @@ export default function WelcomePopup() {
     setShowWelcomePopup
   } = useAppStore();
 
+  const stripIndent = (str: string): string => {
+    try {
+      return str.split("\n").map(l => l.trim()).join('\n')
+    } catch {
+      return str
+    }
+  }
+
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-30">
       <div className="fixed top-0 left-0 w-full h-full bg-white flex items-center justify-center z-40 opacity-50"></div>
@@ -16,6 +26,14 @@ export default function WelcomePopup() {
         <XMarkIcon
           onClick={() => { setShowWelcomePopup(false) }}
           className="w-6 absolute right-2 top-2 cursor-pointer hover:opacity-70" />
+        <Markdown>
+        {stripIndent(`## Timer
+
+        **bold**
+
+          :sunny:
+        `)}
+        </Markdown>
         <p>
           { t("welcome_banner_copy") }
         </p>
