ba07e751842d24c7fdaf2f70289d40cea3d56e4f
Anatoli Makarevich <makaroni4@gmail.com>
2023-11-28 20:00:26 +0100

    Styled dropdown menus

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -5,6 +5,7 @@ import Twemoji from './Twemoji';
 import { useTranslation } from "react-i18next";
 import LanguageMenu from './LanguageMenu';
 import EmojiCountryCodes from '../data/interface';
+import DropdownMenu from './DropdownMenu';
 
 export default function Settings() {
   const { t, i18n } = useTranslation();
@@ -44,24 +45,11 @@ export default function Settings() {
           className="font-bold mb-1 block mr-4">
           { t("i_know") }
         </div>
-        <div
-          className="flex items-center p-1 border-[1px] border-slate-200 hover:border-slate-300 rounded-md cursor-pointer relative"
-          onClick={ () => setUserLanguageMenuVisible(!userLanguageMenuVisible) }>
-          <Twemoji
-            className="mr-3"
-            countryCode={userLanguage} />
 
-          <ChevronDownIcon className='w-4 '/>
-
-          { userLanguageMenuVisible && (
-            <div
-              className="absolute top-full left-0 flex items-center p-1 border-[1px] border-slate-200 hover:border-slate-300 rounded-md
-              bg-white z-30 mt-[-4px] border-t-0">
-              <LanguageMenu
-                excludeLanguage={userLanguage}
-                languageSelected={(language: keyof EmojiCountryCodes) => { setUserLanguage(language); i18n.changeLanguage(language); }} />
-            </div>
-          )}
+        <div>
+          <DropdownMenu
+            excludeLanguage={userLanguage}
+            languageSelected={(language: keyof EmojiCountryCodes) => setUserLanguage(language)} />
         </div>
       </div>
 
@@ -71,22 +59,10 @@ export default function Settings() {
           { t("i_learn") }
         </div>
 
-        <div
-          className="flex items-center p-1 border-[1px] border-slate-200 hover:border-slate-300 rounded-md cursor-pointer relative"
-          onClick={ () => setEmailLanguageMenuVisible(!emailLanguageMenuVisible) }>
-          <Twemoji
-            className="mr-3"
-            countryCode={emailLanguage} />
-          <ChevronDownIcon className='w-4 '/>
-
-          { emailLanguageMenuVisible && (
-            <div className="absolute top-full left-0 flex items-center p-1 border-[1px] border-slate-200 hover:border-slate-300 rounded-md
-            bg-white z-30 mt-[-4px] border-t-0">
-              <LanguageMenu
-                excludeLanguage={emailLanguage}
-                languageSelected={(language: keyof EmojiCountryCodes) => setEmailLanguage(language)} />
-            </div>
-          )}
+        <div>
+          <DropdownMenu
+            excludeLanguage={emailLanguage}
+            languageSelected={(language: keyof EmojiCountryCodes) => setEmailLanguage(language)} />
         </div>
       </div>
     </div>
