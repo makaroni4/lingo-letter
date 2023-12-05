7b590f212958e7ca5af00c13d2710d9caa27ac41
Anatoli Makarevich <makaroni4@gmail.com>
2023-12-05 17:00:46 +0100

    Responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,25 @@
 export default function Footer() {
   return (
     <footer className="container">
-      <hr className="mb-4" />
+      <hr className="mb-4"/>
 
-      <div className="flex justify-between pt-6 pb-10 text-md text-slate-600">
-        <div>
-          Made with ❤️ by <a className="underline" href="https://twitter.com/makaroni4">Anatoli Makarevich</a>
+      <div className="flex flex-col md:flex-row justify-between py-6 text-md text-slate-600">
+        <div className="mb-4 md:mb-0">
+          <div className="mb-3">
+            © {new Date().getFullYear()} Lingo Email
+          </div>
+          <a
+            className="flex mb-3 mr-4 underline"
+            href="https://github.com/makaroni4/lingo-email">
+            <img className="w-4 mr-2" src="./github.svg" alt="Github repo" />Source Code
+          </a>
+          <a
+            className="block underline"
+            href="https://github.com/makaroni4/lingo-email/issues">Feedback</a>
         </div>
 
         <div>
+          Made with ❤️ by <a className="underline" href="https://twitter.com/makaroni4">Anatoli Makarevich</a>
         </div>
       </div>
     </footer>
