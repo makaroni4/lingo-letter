7d8587454e6a51bc12d8f65aaa95fca00827a4fc
Anatoli Makarevich <makaroni4@gmail.com>
2023-12-09 10:11:31 +0100

    Split i18n files into example exams and UI

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,55 @@
 import i18next from "i18next"
 
-import enTranslations from "./i18n/en.json"
-import deTranslations from "./i18n/de.json"
-import esTranslations from "./i18n/es.json"
-import ptTranslations from "./i18n/pt.json"
-import itTranslations from "./i18n/it.json"
-import frTranslations from "./i18n/fr.json"
+import enUITranslations from "./i18n/ui/en.json"
+import deUITranslations from "./i18n/ui/de.json"
+import esUITranslations from "./i18n/ui/es.json"
+import ptUITranslations from "./i18n/ui/pt.json"
+import itUITranslations from "./i18n/ui/it.json"
+import frUITranslations from "./i18n/ui/fr.json"
+
+import enExamsTranslations from "./i18n/exams/en.json"
+import deExamsTranslations from "./i18n/exams/de.json"
+import esExamsTranslations from "./i18n/exams/es.json"
+import ptExamsTranslations from "./i18n/exams/pt.json"
+import itExamsTranslations from "./i18n/exams/it.json"
+import frExamsTranslations from "./i18n/exams/fr.json"
+
+const enTranslations = {
+  translation: {
+    ...enUITranslations.translation,
+    ...enExamsTranslations.translation
+  }
+}
+const deTranslations = {
+  translation: {
+    ...deUITranslations.translation,
+    ...deExamsTranslations.translation
+  }
+}
+const esTranslations = {
+  translation: {
+    ...esUITranslations.translation,
+    ...esExamsTranslations.translation
+  }
+}
+const ptTranslations = {
+  translation: {
+    ...ptUITranslations.translation,
+    ...ptExamsTranslations.translation
+  }
+}
+const itTranslations = {
+  translation: {
+    ...itUITranslations.translation,
+    ...itExamsTranslations.translation
+  }
+}
+const frTranslations = {
+  translation: {
+    ...frUITranslations.translation,
+    ...frExamsTranslations.translation
+  }
+}
 
 i18next.init({
   fallbackLng: "en",
